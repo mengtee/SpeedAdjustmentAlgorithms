16eeac972c81e2e1846d4818d2b9dc1c7bfe6ed0
mengtee <mengtee1127@gmail.com>
2024-06-19 13:38:52 +0800

    updating on roundabout file and python file, prepare to test using dynamic user assignment algorithms

--- roundabout/roundabout.py
+++ roundabout/roundabout.py
@@ -1,3 +1,6 @@
+'''Used a priority system, Used the predicted future states of the vehicles
+to determine if the speed need to be adjusted'''
+
 import optparse
 import os
 import sys
@@ -22,6 +25,7 @@ def get_options():
     return options
 
 def calculate_distance(x1, y1, x2, y2):
+    # Return all the positive values
     return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
 def predict_collision_time(distance1, speed1, distance2, speed2):
@@ -38,15 +42,18 @@ def calculate_speed_adjustments(vehicles):
         pos = traci.vehicle.getPosition(veh)
         speed = traci.vehicle.getSpeed(veh)
         distance_to_junction = calculate_distance(pos[0], pos[1], traci.junction.getPosition("J2")[0], traci.junction.getPosition("J2")[1])
-        time_to_junction = distance_to_junction / speed if speed > 0 else float('inf')
+        time_to_junction = distance_to_junction / speed if speed > 0 else float('inf') 
         vehicle_props[veh] = {"pos": pos, "speed": speed, "distance_to_junction": distance_to_junction, "time_to_junction": time_to_junction}
 
     priorities = {}
-    for veh in vehicle_props:
-        priority = 1 / (vehicle_props[veh]["distance_to_junction"] + 0.1 * vehicle_props[veh]["speed"])
+    for veh in vehicle_props: # Prioritise vehicles that reaching the junction first
+        # priority = 1 / (vehicle_props[veh]["distance_to_junction"] + 0.1 * vehicle_props[veh]["speed"])
+        priority = -vehicle_props[veh]["distance_to_junction"]
         priorities[veh] = priority
 
     sorted_vehicles = sorted(priorities, key=priorities.get, reverse=True)
+
+    print(f'This is the sorted vehicles list {sorted_vehicles}')
 
     horizon = 20  # increased prediction horizon
     dt = 0.01
@@ -73,9 +80,11 @@ def calculate_speed_adjustments(vehicles):
                     if abs(collision_time) < safety_margin:
                         new_speed = max(min_speed, predicted_states[t-1, i, 1] - 0.5 * (predicted_states[t-1, i, 1] - predicted_states[t-1, j, 1]))
                         traci.vehicle.slowDown(veh, new_speed, 5)
+                        print(f"Vehicle id: {veh}, new speed: {new_speed}")
                         break
                 else:
                     traci.vehicle.slowDown(veh, max_speed, 5)
+                    print(f"Vehicle id: {veh}, max speed: {max_speed}")
     return
 
 def run():
@@ -89,11 +98,19 @@ def run():
         
         # if len(vehicles) > 0:
         #     calculate_speed_adjustments(vehicles)
+
+        # # Print out the speed of each vehicle
+        # print(f"Step {step}:")
+        # for vehicle_id in vehicles:
+        #     speed = traci.vehicle.getSpeed(vehicle_id)
+        #     print(f"The Vehicle {vehicle_id}: Speed {speed:.2f} m/s")
+        
         
         step += 1
 
     traci.close()
     sys.stdout.flush()
+
 
 if __name__ == "__main__":
     options = get_options()
